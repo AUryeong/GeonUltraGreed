5167e4f8867becbbb184b24792e111702cd51459
AUryeong <jykee7030@gmail.coma>
2022-04-05 03:28:59 +0900

    asdas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,23 +2,102 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : Singleton<GameManager>
 {
 
+	[Header("[Cursor]")]
 	[SerializeField]
 	Image cursor;
-
 	[SerializeField]
 	Texture2D invisiblecursor;
 
+	[Header("[UI - HP]")]
+	[SerializeField]
+	Image Hpbar;
+	[SerializeField]
+	Image Hpwave;
+	[SerializeField]
+	TextMeshProUGUI Hptext;
+
+	[Header("[UI - Dash]")]
+	[SerializeField]
+	Image Dashbarbase;
+	[SerializeField]
+	Image Dashbarleft;
+	[SerializeField]
+	Image Dashbarright;
+	[SerializeField]
+	List<Image> Dashbars;
+	[SerializeField]
+	Sprite[] Dashbarsprites;
+	int dashcount;
+
+	[Header("[Player]")]
+	[SerializeField]
+	Player player;
+
 	protected override void Awake()
 	{
-		Cursor.SetCursor(invisiblecursor, Vector2.zero, CursorMode.Auto);
 	}
 	void Update()
 	{
 		cursor.transform.position = Input.mousePosition;
+		HealthChange();
+		DashChange();
 	}
 
+	public void DashChange()
+    {
+		if(dashcount != player.maxdash)
+        {
+			int dc = Dashbars.Count;
+			int chai = player.maxdash - dc;
+			if (player.maxdash > dc)
+            {
+				for(int i = 0; i < chai; i++)
+                {
+					Image image = GameObject.Instantiate<Image>(Dashbarbase);
+					image.transform.SetParent(Dashbarleft.transform);
+					image.GetComponent<RectTransform>().anchoredPosition = new Vector3(33 + (i+dc) * 54, 0, 0);
+					image.transform.SetAsLastSibling();
+					Dashbars.Add(image);
+				}
+            }
+			for(int i = 0; i< dc; i++)
+            {
+                if (i < player.maxdash)
+                {
+					Dashbars[i].gameObject.SetActive(true);
+                }
+                else
+				{
+					Dashbars[i].gameObject.SetActive(false);
+				}
+            }
+			Dashbarright.GetComponent<RectTransform>().anchoredPosition = new Vector3(12 + player.maxdash * 54, 0, 0);
+			Dashbarright.transform.SetAsLastSibling();
+			dashcount = player.maxdash;
+        }
+		for(int i = 0; i < player.maxdash; i++)
+        {
+            if (i < player.dash)
+			{
+				Dashbars[i].transform.GetChild(0).gameObject.SetActive(true);
+			}
+            else
+            {
+				Dashbars[i].transform.GetChild(0).gameObject.SetActive(false);
+			}
+        }
+    }
+
+	public void HealthChange()
+    {
+		float f = player.hp / player.maxhp;
+		Hpbar.fillAmount = f;
+		Hpwave.GetComponent<RectTransform>().anchoredPosition = new Vector3(-126f + (300 * f), 0, 0);
+		Hptext.text = player.hp + " / " + player.maxhp;
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,42 +5,112 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private GameObject lookat;
-    [SerializeField]
     private GameObject sprite;
-    public float Speed;
+    private Rigidbody2D rigid;
+
+    public Rigidbody2D Rigid
+    {
+        get
+        {
+            if (rigid == null)
+            {
+                rigid = GetComponent<Rigidbody2D>();
+            }
+            return rigid;
+        }
+    }
+
+    public float speed;
+    public float jumpspeed;
+    public int jumpmax;
+    public float hp;
+    public int maxhp;
+    public int maxdash;
+    public int dash;
+    int jump;
+    public int jumpadd;
+
     void Start()
     {
-        
+        jump = jumpmax;
     }
 
     void Moving()
     {
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * Speed);
-            sprite.transform.localScale = Vector3.one;
+            RaycastHit2D rayhit = Physics2D.Raycast(transform.position, Vector2.right, 0.85f, LayerMask.GetMask("Platform"));
+            RaycastHit2D rayhit2 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 0.7f), Vector2.right, 0.85f, LayerMask.GetMask("Platform"));
+            RaycastHit2D rayhit3 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.7f), Vector2.right, 0.85f, LayerMask.GetMask("Platform"));
+            if (rayhit.collider == null && rayhit2.collider == null && rayhit3.collider == null)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * speed / 100 * 6);
+            }
         }
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * Speed);
-            sprite.transform.localScale = new Vector3(-1, 1, 1);
-        }
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            GetComponent<Rigidbody2D>().AddForce(Vector3.up * Time.deltaTime * 20);
+            RaycastHit2D rayhit = Physics2D.Raycast(transform.position, Vector2.left, 0.85f, LayerMask.GetMask("Platform"));
+            RaycastHit2D rayhit2 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 0.7f), Vector2.left, 0.85f, LayerMask.GetMask("Platform"));
+            RaycastHit2D rayhit3 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.7f), Vector2.left, 0.85f, LayerMask.GetMask("Platform"));
+            if (rayhit.collider == null && rayhit2.collider == null && rayhit3.collider == null)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * speed / 100 * 6);
+            }
         }
     }
 
     void MouseMoving()
     {
         Vector3 vector = Camera.main.ScreenToWorldPoint(Input.mousePosition) - sprite.transform.position;
-        lookat.transform.localPosition = Vector3.Lerp(lookat.transform.localPosition, new Vector3(Mathf.Clamp(vector.normalized.x/2, -0.17f, 0.17f), Mathf.Clamp(vector.normalized.y / 2, -0.12f, 0.12f), 0), Time.deltaTime * 10);
+        sprite.transform.localScale = new Vector3((vector.x < 0) ? -1 : 1, 1, 1);
     }
 
-    void FixedUpdate()
+    void CheckJumping()
     {
-        Moving();
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && jump != 0)
+        {
+            jump--;
+            jumpadd = 200;
+            Rigid.velocity = new Vector2(Rigid.velocity.x, 0);
+            Rigid.AddForce(Vector3.up * 600 * jumpspeed);
+        }
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space)) && jumpadd > 0)
+        {
+            Rigid.AddForce(Vector3.up  * jumpspeed);
+            jumpadd--;
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.Space))
+        {
+            jumpadd = 0;
+        }
+        if (Rigid.velocity.y < 0)
+        {
+            RaycastHit2D rayhit = Physics2D.Raycast(transform.position, Vector2.down, 1.5f, LayerMask.GetMask("Platform"));
+            RaycastHit2D rayhit2 = Physics2D.Raycast(new Vector2(transform.position.x - 0.75f, transform.position.y), Vector2.down, 1.5f, LayerMask.GetMask("Platform"));
+            RaycastHit2D rayhit3 = Physics2D.Raycast(new Vector2(transform.position.x + 0.75f, transform.position.y), Vector2.down, 1.5f, LayerMask.GetMask("Platform"));
+            if (rayhit.collider != null || rayhit2.collider != null || rayhit3.collider != null)
+            {
+                jump = jumpmax;
+            }
+            else if (jump == jumpmax)
+            {
+                jump--;
+            }
+        }
+    }
+
+    void Dashing()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+        }
+    }
+
+    void Update()
+    {
         MouseMoving();
+        CheckJumping();
+        Moving();
+        Dashing();
     }
 }
